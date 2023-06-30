f7a411b15661199254fa057ab36f4a82eea88d83
euricalex <122707746+euricalex@users.noreply.github.com>
2023-06-29 22:49:29 -0700

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,8 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+const EMAIL_KEY = 'email-form-state';
+
 
 const refs = {
   form: document.querySelector('.feedback-form'),
@@ -10,7 +12,7 @@ const refs = {
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
-populateTextArea();
+refs.emailInput.addEventListener('input', throttle(onEmailAreaInput, 500));
 
 function onFormSubmit(event) {
   event.preventDefault();
@@ -18,17 +20,27 @@ function onFormSubmit(event) {
   const email = refs.emailInput.value;
   const message = refs.textarea.value;
 
+  if (!email || !message) {
+    console.log('Заповніть поля!')
+    return;
+  }
+
   event.target.reset();
 
   const formData = { email, message };
   console.log(formData);
 
   localStorage.removeItem(STORAGE_KEY);
+  localStorage.removeItem(EMAIL_KEY);
 }
 
 function onTextAreaInput(event) {
   const message = event.target.value;
   localStorage.setItem(STORAGE_KEY, message);
+}
+function onEmailAreaInput(event) {
+  const email = event.target.value;
+  localStorage.setItem(EMAIL_KEY, email);
 }
 
 function populateTextArea() {
@@ -41,3 +53,14 @@ function populateTextArea() {
     }
    
 }
+function populateEmailArea() {
+  const savedEmail = localStorage.getItem(EMAIL_KEY);
+
+  if (savedEmail) {
+    console.log(savedEmail);
+    refs.emailInput.value = savedEmail;
+  }
+}
+
+populateTextArea();
+populateEmailArea()
